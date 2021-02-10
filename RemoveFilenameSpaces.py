cdcf216df33ee494bd4df791de5e4cedf5524501
Stein <will.stein@yahoo.com>
2021-02-09 20:09:44 -0800

    Update RemoveFilenameSpaces.py

--- RemoveFilenameSpaces.py
+++ RemoveFilenameSpaces.py
@@ -3,16 +3,35 @@ import pathlib
 from pathlib import Path
 
 ignored_directories = ['AppData', 'Downloads', '.vscode']
-fileExtensionsToCheck = ['.py', '.cpp', '.txt']
+extensions_to_check = ['.py', '.cpp', '.txt']
+
+class ChangeName():
+    def __init__(self, logFile):
+        self.logFile = logFile
+    
+    def replaceSpaces(self, file_name):
+        new_name = ''
+        for character in file_name.name:
+            new_name += '_' if character.isspace() else character
+        return new_name
+
+    def commitNameChange(self, file_name, new_name):
+        print('\'{file_name.name}\' changed to \'{new_name}\' at {file_name}')
+
+    def promptUser(self, file_name, new_name):
+        print(f'\nFile found at {file_name}')
+        response = input(f'(Y/N) Would you like to change the name \'{file_name.name}\' to \'{new_name}\'?')
+        return True if response.upper() == 'Y' else False
+
+    def namechange(self, file_name):
+        new_name = self.replaceSpaces(file_name)
+        if self.promptUser(file_name, new_name):
+            self.commitNameChange(file_name, new_name)
+            if self.logFile != None:
+                self.logFile.writelog(f'\'{file_name.name}\' changed to \'{new_name}\' at {file_name}')
 
 def getFileExtension(file_name):
     return pathlib.Path(file_name).suffix
-
-def replaceSpaces(file_name):
-    new_name =''
-    for character in file_name:
-        new_name += '_' if i.isspace() else character
-    return new_name
 
 def checkIfIgnored(current_directory):
     for i in ignored_directories:
@@ -26,27 +45,30 @@ def checkFile(file_name):
             return True
 
 class LogFile:
-    def __init__(self, file):
-        self.logFile = file
+    def __init__(self, logfile):
+        self.logFile = logfile
     
     def writelog(self, text, timestamp=True):
         log = open(self.logFile, 'at')
         log.write(f'[{datetime.datetime.now()}] {text}\n') if timestamp else log.write(f'{text} \n')
         log.close()
 
-def main(current_directory, changelog, ignored_directories, fileExtensionsToCheck):
+def main(current_directory, filenamechange, ignored_directories, extensions_to_check):
     if checkIfIgnored(current_directory):
         try:
-            for file in current_directory.iterdir():
-                if file.is_dir():
-                    main(file, changelog, ignored_directories, fileExtensionsToCheck)
+            for _file in current_directory.iterdir():
+                if _file.is_dir():
+                    main(_file, filenamechange, ignored_directories, extensions_to_check)
                 else:
-                    if getFileExtension(file.name) in fileExtensionsToCheck:
-                        if checkFile(file):
-                            changelog.writelog(f'\'{file.name}\' changed to \'{file.name}\' at {file}')
-        except:
-            pass
+                    if getFileExtension(_file.name) in extensions_to_check:
+                        if checkFile(_file):
+                            filenamechange.namechange(_file)
+        except Exception as error:
+            print(error)
 
 if __name__ == "__main__":
     changelog = LogFile('NameChangeLog.txt')
-    main(Path.home(), changelog, ignored_directories, fileExtensionsToCheck)
+    filenamechange = ChangeName(changelog)
+    print('Searching files...')
+    main(Path.home(), filenamechange, ignored_directories, extensions_to_check)
+    input('Press any key to exit.')
